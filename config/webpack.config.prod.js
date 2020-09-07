55a9cd6ea054dda8b43cd16484f46adaeeab5fa6
jsCoder <huangsongsong@innotechx.com>
2020-09-07 17:34:58 +0800

    config: 加入UglifyJsPlugin，启用代码压缩

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
 const baseConfig = require('./webpack.config.base');
 
 const prodConfig = {
@@ -29,6 +31,26 @@ const prodConfig = {
   },
   plugins: [
     new webpack.optimize.ModuleConcatenationPlugin(),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        // 在UglifyJs删除没有用到的代码时不输出警告
+        warnings: false,
+        compress: {
+          // 删除所有的 `console` 语句，可以兼容ie浏览器
+          drop_console: true,
+          // 内嵌定义了但是只用到一次的变量
+          collapse_vars: true,
+          // 提取出出现多次但是没有定义成变量去引用的静态值
+          reduce_vars: true,
+        },
+        output: {
+          // 最紧凑的输出
+          beautify: false,
+          // 删除所有的注释
+          comments: false,
+        }
+      },
+    }),
   ],
 }
 
